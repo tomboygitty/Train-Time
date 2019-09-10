e6543821198752bcb9edd5ef4c2d18515c78e76e
Tom Elliott <xenomus@gmail.com>
2019-09-10 18:44:29 -0500

    js logic main stuff done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,29 +26,11 @@ $("#add-train").on("click", function() {
     first = $("#time-input").val().trim();
     freq = $("#freq-input").val().trim();
     
-    // First Time (pushed back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(first, "HH:mm").subtract(1, "years");
-    console.log(firstTimeConverted);
-
-    // Current Time
-    var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
-
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
-
-    // Time apart (remainder)
-    var tRemainder = diffTime % freq;
-    console.log(tRemainder);
-
-    // Minutes Until Train
-    mins = freq - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + mins);
+    mins = calcMins(first, freq);
 
     // Next Train
-    next = moment().add(mins, "minutes");
-    console.log("ARRIVAL TIME: " + moment(next).format("HH:mm"));
+    next = moment(moment().add(mins, "minutes")).format("HH:mm A");
+    //console.log("ARRIVAL TIME: " + next);
     
 
     if ((name!="")&&(dest!="")&&(first!="")&&(freq!="")&&(moment(first, 'HH:mm', true).isValid())) {
@@ -57,8 +39,8 @@ $("#add-train").on("click", function() {
             destination: dest,
             firstTime: first,
             frequency: freq,
-            nextArrival: mins,
-            minsAway: next,
+            nextArrival: next,
+            minsAway: mins,
             dateAdded: firebase.database.ServerValue.TIMESTAMP
         });
     }
@@ -71,20 +53,15 @@ $("#add-train").on("click", function() {
 database.ref().on("child_added", function(snapshot) {
     var sv = snapshot.val();
 
-    // console.log(sv.name);
-    // console.log(sv.role);
-    // console.log(sv.date);
-    // console.log(sv.monthsWorked);
-    // console.log(sv.rate);
-    // console.log(sv.totalBilled);
-
     var tableItem = $("<tr>");
     var tableName = $("<td>");
     var tableDest = $("<td>");
     var tableFreq = $("<td>");
     var tableNext = $("<td>");
+    tableNext.attr("id", "next" + sv.name);
     var tableMins = $("<td>");
-
+    tableMins.attr("id", "mins" + sv.name);
+    console.log(tableNext.attr("id") + ", " + tableMins.attr("id"));
     
     
     tableName.text(sv.name);
@@ -94,13 +71,52 @@ database.ref().on("child_added", function(snapshot) {
     tableMins.text(sv.minsAway);
 
     tableItem.append(tableName);
-    tableItem.append(tableRole);
-    tableItem.append(tableDate);
-    tableItem.append(tableWorked);
-    tableItem.append(tableRate);
+    tableItem.append(tableDest);
+    tableItem.append(tableFreq);
+    tableItem.append(tableNext);
+    tableItem.append(tableMins);
 
     $("#table-body").append(tableItem);
 
 }, function(errorObject) {
-  console.log("Errors handled: " + errorObject.code);
+    console.log("Errors handled: " + errorObject.code);
 });
+
+function calcMins(start, freq) {
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(start, "HH:mm").subtract(1, "years");
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % freq;
+
+    // Minutes Until Train
+    return mins = freq - tRemainder;
+}
+
+function updateTimes() {
+    var query = firebase.database().ref().orderByKey();
+    query.once("value")
+    .then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+        // childData will be the actual contents of the child
+        var childData = childSnapshot.val();
+        var childFirst = childData.firstTime;
+        var childFreq = childData.frequency;
+        var childMins = calcMins(childFirst, childFreq);
+        var childNext = moment(moment().add(childMins, "minutes")).format("HH:mm A");
+        var nextID = "#next" + childData.name;
+        var minsID = "#mins" + childData.name;
+        $(nextID).text(childNext);
+        $(minsID).text(childMins);
+        console.log(childFirst);
+        console.log(childFreq);
+        console.log(childNext);
+        console.log(childMins);
+        });
+    });
+}
+
+updateTimes();
